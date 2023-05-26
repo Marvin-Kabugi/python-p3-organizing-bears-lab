f3e4a8788577401ed7e722e1bdbe4012a96226fb
Marvin-Kabugi <kabugijeff@gmail.com>
2023-05-26 11:49:20 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, sex) VALUES 
+    ('Mr. Chocolate', 'M'),
+    ('Rowdy', 'M'),
+    ('Tabitha', 'F'),
+    ('Seargent Brown', 'M'),
+    ('Melissa', 'F'),
+    ('Grinch', 'M'),
+    ('Wendy', 'F'),
+    (NULL, 'M');
+
+    
